ff370793759171a3dc9f44044fb406e197eca7f9
markruangrattham <mruangrattham@ucsd.edu>
2022-05-01 17:24:41 -0700

    Added junit and function

---
+++ skilldemotester.java
@@ -0,0 +1,12 @@
+import java.beans.Transient;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemotester {
+    @Test
+    public void Test1(){
+        assertEquals(2, skilldemo.Subtrack(4,2));
+        
+    }
+}
